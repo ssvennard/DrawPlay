6f85642f74c44c00a46f7e2db423d73f8355ed3b
Michael Ruiz <mruiz1@mines.edu>
2019-09-26 10:15:24 -0600

    Moved Meow again to account for increased head size, also filled mouth height and width

--- src/Cat.java
+++ src/Cat.java
@@ -14,8 +14,8 @@ public class Cat {
 	private static final int EYE_HEIGHT = 10;
 	private static final int EYE_WIDTH = 20;
 	// pick mouth height, width is based on head dimension
-	private static final int MOUTH_HEIGHT = 10;
-	private static final int MOUTH_WIDTH = HEAD_DIMENSION/4;
+	private static final int MOUTH_HEIGHT = HEAD_DIMENSION/4;
+	private static final int MOUTH_WIDTH = 10;
 	// mouth starts about 40% from left edge of head
 	private static final int MOUTH_X = HEAD_DIMENSION/5 * 2;
 	private static final int MOUTH_Y = HEAD_DIMENSION/5 * 3;
@@ -44,6 +44,6 @@ public class Cat {
 		g2.setColor(Color.black);
 		// Meow text appears below cat head, +10 places below 
 		// so it doesn't overlap the drawing
-		g2.drawString("Meow", catX+35, catY+HEAD_DIMENSION+10);	
+		g2.drawString("Meow", catX+110, catY+HEAD_DIMENSION+10);	
 	}
 }
